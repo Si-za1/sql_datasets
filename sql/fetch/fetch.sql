0ac126ea5a57fe16235ea2ecbe50ce0af8e15f38
Siza <laurenadhikari16@gmail.com>
2023-06-27 14:16:23 +0545

    new way updated for the mentioned 2 queries

--- sql/fetch/fetch.sql
+++ sql/fetch/fetch.sql
@@ -17,22 +17,43 @@ GROUP BY customer_id;
 
 -- first item purchased by customer A
 
-SELECT m.customer_id, MIN(s.order_date) AS first_purchase_date, menu.product_name AS first_item_purchased
-FROM sales s
-JOIN members m ON s.customer_id = m.customer_id
-JOIN menu ON s.product_id = menu.product_id
-WHERE m.customer_id = 'A'
-GROUP BY m.customer_id, menu.product_name;
+-- SELECT m.customer_id, MIN(s.order_date) AS first_purchase_date, menu.product_name AS first_item_purchased
+-- FROM sales s
+-- JOIN members m ON s.customer_id = m.customer_id
+-- JOIN menu ON s.product_id = menu.product_id
+-- WHERE m.customer_id = 'A'
+-- GROUP BY m.customer_id, menu.product_name;
+
+SELECT menu.product_name, MIN(sales.order_date) AS first_purchase_date
+FROM sales
+JOIN menu ON sales.product_id = menu.product_id
+WHERE sales.customer_id = 'A'
+GROUP BY menu.product_name
+HAVING MIN(sales.order_date) = (
+    SELECT MIN(order_date)
+    FROM sales
+    WHERE customer_id = 'A'
+)
 
 
 -- What is the most purchased item on the menu and how many times purchased 
 
+-- SELECT menu.product_name AS most_purchased_item, COUNT(*) AS purchase_count
+-- FROM sales
+-- JOIN menu ON sales.product_id = menu.product_id
+-- GROUP BY menu.product_name
+-- ORDER BY purchase_count DESC
+-- LIMIT 1;
+
 SELECT menu.product_name AS most_purchased_item, COUNT(*) AS purchase_count
-FROM sales
-JOIN menu ON sales.product_id = menu.product_id
-GROUP BY menu.product_name
-ORDER BY purchase_count DESC
-LIMIT 1;
+FROM (
+	SELECT TOP 1 
+	FROM sales
+	JOIN menu ON sales.product_id = menu.product_id
+	 ) as m
+
+GROUP BY  menu.product_name
+ORDER BY purchase_count DESC;
 
 
 
